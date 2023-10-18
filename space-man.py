26fa60f1ddb9c43dc4f2b46f622575840afcc76d
EmsR434 <142956103+EmsR434@users.noreply.github.com>
2023-10-18 00:03:40 -0700

    Update space-man.py

--- space-man.py
+++ space-man.py
@@ -3,58 +3,53 @@ import random
 #welcome the user
 name = input("What is your name? ")
 print("Hello, " + name, "Time to play!")
-print("Start guessing...")
 
 #select any word to play with
-words = ['swoon', 'charm', 'gentleman', 'swoop', 'alarm']
-word = random.choice(words)
+word_list = ['swoon', 'charm', 'gentleman', 'swoop', 'alarm']
 
-guesses = ''
+#code has to choose a word
+def choose_random_word():
+    return random.choice(word_list)
 
-#determine the number of turns
-turns = 10
+def play_game():
+    word_to_guess = choose_random_word()
+    guessed_letters = []
+    #choose how many turns 
+    attempts = 10
 
-#create a loop
+    while attempts > 0:
+        display_word = ""
+        for letter in word_to_guess:
+            if letter in guessed_letters:
+                display_word += letter
+            else:
+                display_word += "_"
+        
+        print(f"Word: {display_word}")
+        print(f"Attempts left: {attempts}")
 
-#check if the turns are more than zero
-while turns > 0:
+        guess = input("Guess a letter: ").lower()
 
-    failed = 0
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single letter.")
+            continue
+        if guess in guessed_letters:
+            print("You already guessed that letter.")
+            continue
+        guessed_letters.append(guess)
 
-    for char in word:
-
-        #see if the character is in the players guess
-        if char in guesses:
-            
-            #print then out the character
-            print (char,end=""),
+        if guess in word_to_guess:
+            print("Good guess!")
+            if set(guessed_letters) == set(word_to_guess):
+                print(f"Congratulations! You guessed the word: (word_to_guess)")
+                break
         else:
-            print("_",end="")
-            failed += 1
+            print("Incorrect guess.")
+            attempts -= 1
+        
+    if attempts == 0:
+        print(f"Sorry, you're out of tries. The word was: (word_to_guess)")
+if __name__ == "__main__":
+    play_game()
+    
 
-
-    if failed == 0:
-        print("You Won!")
-        print("The word is:", word)
-        break
-
-#ask the user go guess a character
-guess = input("guess a character")
-
-#set the players guess to guesses
-guesses += guess
-
-#if the guess is not found in the secret word
-if guess not in word:
-
-    #turns counter decreases with 1 (now 9)
-    turns -= 1
-
-    print ("Wrong")
-
-    #how many turns are left
-    print("You have", + turns, 'more guesses')
-
-    if turns == 0:
-
-        print("You Lose")
